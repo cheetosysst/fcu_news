396e8cdb33db55d3ae09d8e1f4c65a9832b02cfb
cheetosysst <x9930707@gmail.com>
2020-12-29 15:49:26 +0800

    Delete unused code

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ from PyQt5 import QtWidgets, uic
 import sys
 
 app = QtWidgets.QApplication(sys.argv)
-global sWidth, sHeight
 sWidth  = app.primaryScreen().availableGeometry().width()
 sHeight = app.primaryScreen().availableGeometry().height()
 window = Ui(sWidth*2/3,sHeight*2/3)
